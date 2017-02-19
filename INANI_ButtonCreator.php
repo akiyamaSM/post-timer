b581b93b84dc53e25c7ccc9f59b6b52a1b8dc982
unknown <inanielhoussain@gmail.com>
2017-02-19 13:25:17 +0000

    Add option for displaying the widget

--- INANI_ButtonCreator.php
+++ INANI_ButtonCreator.php
@@ -11,6 +11,7 @@ class INANI_ButtonCreator
     public function run()
     {
         add_action('add_meta_boxes', [$this, 'add_meta_box']);
+        add_action('save_post', [$this, 'save_post_timer_option']);
     }
 
     /**
@@ -38,6 +39,35 @@ class INANI_ButtonCreator
         require_once(
             plugin_dir_path(__FILE__). 'views/checkbox.php'
         );
+    }
+
+    /**
+     * Callback function for saving custom field
+     *
+     * @param $post_id
+     */
+    public function save_post_timer_option($post_id)
+    {
+        if(! $this->user_can_save($post_id)){
+            return ;
+        }
+
+        $post_timer = $_POST['show_timer'];
+        $affect = !is_null($post_timer)? 1 : 0;
+        update_post_meta($post_id, 'inani_show_timer', $affect);
+    }
+
+    /**
+     * Check if the user can save
+     *
+     * @param $post_id
+     * @return bool
+     */
+    public function user_can_save($post_id)
+    {
+        $autosave = wp_is_post_autosave($post_id);
+        $revision = wp_is_post_revision($post_id);
+        return !($autosave || $revision);
     }
 }
 
--- views/checkbox.php
+++ views/checkbox.php
@@ -1,3 +1,18 @@
+<?php
+
+function checkTheOption($post_id)
+{
+  $result = get_post_meta($post_id, 'inani_show_timer', true);
+  if(isset($result) && $result == 1)
+      echo "checked";
+}
+?>
+
 <label class="selectit">
-    <input value="1" name="show_timer" id="show_timer" checked="checked" type="checkbox"> Show Timer
+    <input
+        value="1"
+        name="show_timer"
+        id="show_timer"
+        <?php checkTheOption(get_the_ID()); ?>
+        type="checkbox"> Show Timer
 </label>
